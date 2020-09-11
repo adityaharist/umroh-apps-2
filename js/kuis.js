5075429339ca07e7a132208f336c001782f89cd3
ari firmansyah <36336485+arifirmansyah@users.noreply.github.com>
2020-09-11 07:37:29 +0700

    Add files via upload

--- js/kuis.js
+++ js/kuis.js
@@ -3,54 +3,58 @@
 //daftar pertanyaan, jawaban tersedia nomor 0-3 (index)
 var state = {
   questions: [{
-      question: 'Apakah Hukum asli dari Ibadah Umroh?',
-      answers: ['Wajib', 'Sunnah', 'Mubah', 'Makruh'],
-      answerCorrect: 1
+      question: 'Tujuan dilaksanakan ibadah haji?',
+      answers: ['Mengunjungi Baitullah di Mekah kerana mengerjakan ibadat tertentu pada masa tertentu dengan syarat tertentu', 'Mengunjungi Kaabah di Mekah kerana melakukan tawaf kerana Allah', 'Mengunjungi Baitullah di Mekah kerana melakukan ibadat tertentu pada waktu tertentu.', 'Mengunjungi Baitullah di Mekah kerana ingin melakukan tawaf, saie dan bercukur'],
+      answerCorrect: 0
     },
     {
-      question: 'Mana yang bukan rukun Umroh?',
-      answers: ['Ihram', 'Tawaf', 'Wudhu', 'Sai'],
+      question: 'Apakah hukum menunaikan haji?',
+      answers: ['Harus', 'Fardhu kifayah', 'Wajib bagi yang mampu', 'Sunat'],
       answerCorrect: 2
     },
     {
-      question: 'Mana yang bukan larangan Ihram?',
-      answers: ['Kentut', 'Berburu', 'Cukur', 'Memakai Cadar'],
-      answerCorrect: 0
-    },
-    {
-      question: 'Manakah keutamaan Umroh dibawah ini yang benar?',
-      answers: ['Mendapat Uang', 'Dipanggil Ustadz', 'Menjadi orang Dermawan', 'Menjadi Tamu Allah'],
-      answerCorrect: 3
-    },
-    {
-      question: 'Berikut ini tempat-tempat yang berada di Masjidil Haram, kecuali?',
-      answers: ['Ka"bah', 'Raudhah', 'Hijir Islmail', 'Maqam Ibrahim'],
+      question: 'Diantara berikut ini, manakah bukan merupakan rukun haji?',
+      answers: ['Sai', 'Tertib', 'Wukuf', 'Melontar Jamratul Aqabah'],
       answerCorrect: 1
     },
     {
-      question: 'Dibawah ini yang bukan praktik ketika berada di Hajar Aswad adalah?',
-      answers: ['Isyarah', 'Mengusap', 'Mencium', 'Menepuk'],
-      answerCorrect: 3
-    },
-    {
-      question: 'Berapakah hitungan putaran Tawaf?',
-      answers: ['6 kali', '7 kali', '5 kali', '8 kali'],
-      answerCorrect: 1
-    },
-    {
-      question: 'Ketika selesai melaksanakan Sai, maka jamaah melakukan?',
-      answers: ['Tahalul', 'Tawaf', 'Sholat', 'Berdoa'],
-      answerCorrect: 0
-    },
-    {
-      question: 'Tempat terakhir pelaksanaan Sai adalah?',
-      answers: ['Shafa', 'Hijir Ismail', 'Marwa', 'Hajar Aswad'],
+      question: 'Tanggal waktu dilaksanakannya wukuf bagi umat Islam ialah',
+      answers: ['10 Zulhijjah', '10 Zulhijjah', '9 Zulhijjah', '10 Zulkaedah'],
       answerCorrect: 2
     },
     {
-      question: 'Dibawah ini yang bukan area Masjid Nabawi adalah?',
-      answers: ['Hajar Aswad', 'Raudhah', 'Makam Rosul', 'Kubah Hijau'],
+      question: 'Berikut, manakah yang bukan merupakan syarat-syarat dalam melakukan ibadah haji :',
+      answers: ['Ada kesempatan waktu', 'Mempunyai ilmu yang luas tentang haji', 'Ada kendaraan untuk pulang dan pergi', 'Mempunyai nafkah yang cukup untuk tanggungan yang ditinggalkan'],
       answerCorrect: 0
+    },
+    {
+      question: 'Berikut ini, manakah yang merupakan larangan saat mengerjakan haji?',
+      answers: ['Menanam tanaman di Mekah', 'Menutup kepala bagi laki-laki', 'Memakai obat-obatan terlarang', 'Menutup tapak tangan bagi perempuan'],
+      answerCorrect: 2
+    },
+    {
+      question: 'Apakah yang dimaksud dengan tawaf ifadhah?',
+      answers: ['Tawaf selamat datang', 'Tawaf selamat tinggal', 'Tawaf sunat yang boleh dilakukan sewaktu-waktu', 'Rukun Tawaf yang wajib dikerjakan oleh semua jemaah haji'],
+      answerCorrect: 2
+    },
+    {
+      question: 'Berikut adalah perkara sunat yang dilakukan sebelum niat ihram haji kecuali :',
+      answers: ['Pakai pakaian ihram berwarna putih',
+        'Memohon maaf atas kesalahan yang pernah dilakukan terhadap orang lain',
+        'Memotong kuku',
+        'Memakai minyak rambut dan bau-bauan'
+      ],
+      answerCorrect: 2
+    },
+    {
+      question: 'Jemaah yang melakukan haji secara tamattuk atau qiran dikenakan dam berupa?',
+      answers: ['Tertib dan taqdir', 'Tertib dan takdil', 'Takhyir dan taqdir', 'Takhyir dan takdil'],
+      answerCorrect: 2
+    },
+    {
+      question: 'Berikut adalah rukun umrah kecuali',
+      answers: ['Wukuf', 'Tawaf', 'Sai', 'Tertib'],
+      answerCorrect: 3
     }
   ],
 
@@ -62,18 +66,9 @@ var state = {
 function clickStart() {
   $('.js-startPage').on('click', 'button', function(event) {
 
-//     $('.js-startPage').remove();
-    $('.js-startPage').addClass('hidden');
+    $('.js-startPage').remove();
     $('#question-container').removeClass('hidden');
     $('#petunjuk').removeClass('hidden');
-  })
-};
-
-function clickRestart() {
-  $('#question-container').on('click', 'button', function(event) {
-	  $('.js-startPage').removeClass('hidden');
-	  $('#question-container').addClass('hidden');
-	  $('#petunjuk').addClass('hidden');
   })
 };
 
@@ -86,7 +81,7 @@ function clickAnswer(chosenElement, state) {
   //jika memilik jawaban yang BENAR, maka akan mengatakan pada pengguna "CORRECT" daripada "WRONG"
   if (chosenAnswer == state.questions[state.currentQuestion].answerCorrect) {
     state.userScore += 1;
-    $('.response1').text('Benar :)');
+    $('.response1').text('Mantul!');
   } else {
     $('.response1').text('Salah :(');
 
@@ -121,16 +116,14 @@ function clickContinue(state) {
   $('.js-continue').addClass('hidden');
   $('.result').addClass('hidden');
   $('section').remove();
-  $("section").hide("fast");
 
   //kuis berakhir dan menampilkan pesan serta membuang jumlah pertanyaan serta nilai dari halaman bawah
   if (state.currentQuestion > 9) {
-//     $('.js-startPage').addClass();
+    // $('.js-startPage').addClass();
     $('#tab-1').append("<div class='row' id='question-container'> <section class = 'question-container col-8'>" + 
                         '<div class="end">Anda telah selesai!</div> <p class ="endScore">Skor anda ' + 
                           state.userScore + " dari " + state.currentQuestion + "<p>");
-//     $('#tab-1').append('<a class="balik button button-raised" onclick="clickStart();">Lagi?!</a>' + "</section> </div>");
-//     $('#tab-1').append('<button class="button button-raised js-start hover" onclick="clickRestart();">Tes ulang</button>' + "</section> </div>");
+    $('#tab-1').append('<a class="balik button button-raised" href="/materi-panduan/">Ke Materi Panduan</a>' + "</section> </div>");
     $('.js-count').remove(); //hapus class js-count
     $('.js-score').remove(); //hapus class js-score
 
@@ -151,6 +144,10 @@ function clickContinue(state) {
 
 }
 
+function random(argument) {
+  // body...
+}
+
 
 // semua fungsi js ini, akan berjalan dengan perintah ini
 // pertama mengambil fungsi clickstart() -> lalu jika tombol pertanyaan diklik -> lalu tombol 'continue' diklik
@@ -167,8 +164,3 @@ $(function() {
   });
 
 });
-
-$$.getJSON(‘http://sitedatasource.com/data 99’, function(data) {
-// specific code related to the data download
-$$("#cover").hide();
-}
