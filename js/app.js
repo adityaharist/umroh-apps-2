942a0a37caa48ba0083e1fdc2c3fd44264d0894d
Aditya Harist <jumphigher19@gmail.com>
2021-06-25 21:56:32 +0700

    y

--- js/app.js
+++ js/app.js
@@ -59,6 +59,11 @@ if (document.location.search.indexOf('theme=') >= 0) {
   theme = document.location.search.split('theme=')[1].split('&')[0];
 }
 
+// Alert
+$('.open-alert').on('click', function () {
+  app.dialog.alert('Kamu harus pilih Levelnya!');
+});
+
 // Init App
 var app = new Framework7({
   id: 'io.framework7.umrohapp',
